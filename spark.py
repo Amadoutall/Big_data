131291928f59488fd12d3c0470761d972ec71b1e
Amadou Tall <amadoutall4646@gmail.com>
2020-12-11 20:25:00 +0100

    update file

--- spark.py
+++ spark.py
@@ -1,3 +1,8 @@
+###### TP1: Outils BIG DATA ######
+# Noms des membres du groupe
+# AMADOU Tall
+# BAMBOU Regis Aymar
+
 # import package
 from pyspark import SparkContext
 
@@ -8,7 +13,12 @@ file = sc.textFile("sample.txt")
 # regroupement mot par mot
 words = file.flatMap(lambda line : line.split(" "))
 wordsCounts= words.countByValue()
+
 # affichage
 for word, count in wordsCounts.items():
     print("{} :{}".format(word,count))
+
+# exportation
+# rdd=sc.parallelize(wordsCounts)
+# rdd.coalesce(1).saveAsTextFile("base.txt")
 
