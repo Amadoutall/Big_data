4b607de141da67d5695d703d2de0797b4866e96c
Amadou Tall <amadoutall4646@gmail.com>
2020-12-11 01:54:05 +0100

    update file

--- spark.py
+++ spark.py
@@ -1,14 +1,14 @@
 # import package
 from pyspark import SparkContext
 
-# Création de la session sparkContext
+# Creation de la session sparkContext
 sc = SparkContext(master="local",appName="Spark Demo")
 # importation du fichier sample.txt
 file = sc.textFile("sample.txt")
 # regroupement mot par mot
 words = file.flatMap(lambda line : line.split(" "))
 wordsCounts= words.countByValue()
-# affichage,
+# affichage
 for word, count in wordsCounts.items():
     print("{} :{}".format(word,count))
 
